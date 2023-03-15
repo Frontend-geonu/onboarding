be0b85af8f3c28edeb16e9b5badf13b0ad65eb80
front_김건우 <kim.geonu@cashwalk.io>
2023-03-15 11:12:22 +0900

    feat : 추천친구 확인기능 추가

--- src/app.feature/ranking/component/recommandFriend/index.tsx
+++ src/app.feature/ranking/component/recommandFriend/index.tsx
@@ -1,5 +1,28 @@
+import * as S from './style';
+import { MdReplay } from 'react-icons/md';
+import { FaSadTear } from 'react-icons/fa';
+import BigButton from '@/app.components/bigButton';
+
 const RecommandFriend: React.FC = () => {
-  return <div>recommandFriend</div>;
+  return (
+    <S.Wrapper>
+      <S.Top>
+        <S.Writing>지금 친구신청이 가능한 친구를 소개합니다</S.Writing>
+        <MdReplay size={30} />
+      </S.Top>
+      <S.Bottom>
+        <S.Nothing>
+          <FaSadTear size={40} />
+          <div>
+            추천할 친구가 없어요.
+            <br />
+            새로고침하여 새로운 추천 친구 목록을 불러오세요!
+          </div>
+        </S.Nothing>
+        <BigButton content="더 많은 친구 보러가기" />
+      </S.Bottom>
+    </S.Wrapper>
+  );
 };
 
 export default RecommandFriend;
---
+++ src/app.feature/ranking/component/recommandFriend/style.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div``;
+
+export const Top = styled.div`
+  background: rgb(231, 231, 231);
+
+  padding: 8px 16px;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const Writing = styled.div``;
+
+export const Bottom = styled.div`
+  padding: 16px;
+  text-align: center;
+`;
+
+export const Nothing = styled.div`
+  margin: 24px 0px;
+`;
