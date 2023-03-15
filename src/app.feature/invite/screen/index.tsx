050dcde66cae7b231d7b7b34b5e0e48c902d07c9
front_김건우 <kim.geonu@cashwalk.io>
2023-03-15 14:51:44 +0900

    feat(friendInvite) : 친구초대 페이지 추가

--- src/app.feature/invite/screen/index.tsx
+++ src/app.feature/invite/screen/index.tsx
@@ -1,4 +1,39 @@
+import * as S from './style';
+
 const Invite: React.FC = () => {
-  return <div>invite</div>;
+  return (
+    <S.Wrapper>
+      <S.Top>
+        <img src="/images/img-addfriend-text.png" alt="addfriend-text" />
+      </S.Top>
+      <S.Middle>
+        <S.Social>
+          <S.IconContainer>
+            <S.Icon src="/images/ic-kakaotalk.png" alt="kakaotalk" />
+            카카오톡
+          </S.IconContainer>
+          <S.IconContainer>
+            <S.Icon src="/images/ic-band.png" alt="band" />
+            밴드
+          </S.IconContainer>
+          <S.IconContainer>
+            <S.Icon src="/images/ic-facebook.png" alt="facebook" />
+            페이스북
+          </S.IconContainer>
+          <S.IconContainer>
+            <S.Icon src="" alt="기타icon" />
+            기타
+          </S.IconContainer>
+        </S.Social>
+        <S.RecommandCodeContainer>
+          <S.RecommandCodeTitle>내 추천코드</S.RecommandCodeTitle>
+          <S.RecommandCode>KRAGUC8</S.RecommandCode>
+          <S.RecommandCodeCopy>코드 복사하기</S.RecommandCodeCopy>
+        </S.RecommandCodeContainer>
+        <img src="/images/img-addfriend-illust.png" alt="addfriend-illust" />
+      </S.Middle>
+      <S.Bottom>이용 안내사항</S.Bottom>
+    </S.Wrapper>
+  );
 };
 export default Invite;
---
+++ src/app.feature/invite/screen/style.ts
@@ -0,0 +1,67 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div``;
+
+export const Top = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 16px;
+`;
+
+export const Middle = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  background: #371804;
+  border-radius: 16px;
+
+  padding: 24px 16px;
+
+  color: white;
+`;
+
+export const Social = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+`;
+
+export const IconContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 4px;
+
+  font-size: 0.75rem;
+`;
+
+export const Icon = styled.img`
+  border-radius: 100px;
+  width: 48px;
+`;
+
+export const RecommandCodeContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #251b02;
+  padding: 16px;
+  border-radius: 8px;
+`;
+
+export const RecommandCodeTitle = styled.div``;
+export const RecommandCode = styled.div`
+  font-size: 28px;
+  font-weight: 500;
+`;
+export const RecommandCodeCopy = styled.div`
+  cursor: pointer;
+  text-decoration: underline;
+  color: yellow;
+
+  font-weight: 600;
+`;
+
+export const Bottom = styled.div`
+  padding: 16px;
+`;
