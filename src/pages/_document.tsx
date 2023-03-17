5b229d61e7df6e9e00429c1d8f4758cd0765a437
front_김건우 <kim.geonu@cashwalk.io>
2023-03-17 11:04:35 +0900

    chore : 사용하지 않는 라이브러리 제거 및 dev용 라이브러리 위치변경

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,23 +1,12 @@
-import { createTheme, ThemeProvider } from '@mui/material';
 import { Html, Head, Main, NextScript } from 'next/document';
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#111111',
-    },
-  },
-});
 
 const Document: React.FC = () => {
   return (
     <Html lang="en">
       <Head />
       <body>
-        <ThemeProvider theme={theme}>
-          <Main />
-          <NextScript />
-        </ThemeProvider>
+        <Main />
+        <NextScript />
       </body>
     </Html>
   );
