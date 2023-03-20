e21eac0d6502c44750f671cde592a83ac1eab3e8
front_geonu <kim.geonu@cashwalk.io>
2023-03-20 10:05:34 +0900

    refactor : 친구초대 페이지 리뷰사항 반영

--- src/app.feature/friend/screen/ScreenFriend.tsx
+++ src/app.feature/friend/screen/ScreenFriend.tsx
@@ -8,46 +8,21 @@ import useLoading from '@/app.hooks/loadingHook';
 import styled from 'styled-components';
 import Loading from '@/app.components/loading/CommonLoading';
 
-export const Wrapper = styled.div``;
-
-export const Main = styled.div`
-  text-align: center;
-  margin-top: 12px;
-`;
-
-export const ButtonContainer = styled.div`
-  margin: 12px 0px;
-`;
-
-export const Notice = styled.div`
-  padding: 36px 0px;
-  background: white;
-
-  font-size: 1.125rem;
-  color: var(--heavy-gray);
-`;
-
-export const LoadingContainer = styled.div`
-  text-align: center;
-
-  // TODO : 로딩컴포넌트의 세로 위치 잡기
-`;
-
 const ScreenFriend: React.FC = () => {
   const [isLoading, handleReset] = useLoading();
 
   return (
-    <Wrapper>
+    <StyledWrapper>
       <Search />
-      <Main>
-        <Notice>
+      <main>
+        <div className="notice">
           친구하고 싶은 사용자를 추천코드로 검색합니다. <br />
           이미 친구인 사용자를 검색하고
           <br /> 싶다면 아래의 버튼을 선택해주세요
-          <ButtonContainer>
+          <span className="button-wrapper">
             <Button content="친구 검색하러 가기" />
-          </ButtonContainer>
-        </Notice>
+          </span>
+        </div>
         {isLoading ? (
           <>
             <ResetRecommandList
@@ -63,18 +38,37 @@ const ScreenFriend: React.FC = () => {
               isLoading={isLoading}
               handleReset={handleReset}
             />
-            <Notice>
+            <div className="notice">
               <FaSadTear size={40} />
               <div>
                 추천할 친구가 없어요.
                 <br />
                 새로고침하여 새로운 추천 친구 목록을 불러오세요!
               </div>
-            </Notice>
+            </div>
           </>
         )}
-      </Main>
-    </Wrapper>
+      </main>
+    </StyledWrapper>
   );
 };
 export default ScreenFriend;
+
+const StyledWrapper = styled.div`
+  main {
+    text-align: center;
+    margin-top: 12px;
+  }
+
+  .button-wrapper {
+    margin: 12px 0px;
+  }
+
+  .notice {
+    padding: 36px 0px;
+    background: white;
+
+    font-size: 1.125rem;
+    color: var(--heavy-gray);
+  }
+`;
